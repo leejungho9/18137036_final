b1eb330eb8fb1a0eaea9ef57020ba11711ece027
18137036 <9wjdgh9@naver.com>
2020-12-03 20:50:57 +0900

    update

---
+++ app/src/main/java/com/example/a18137036_final/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a18137036_final;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a18137036_final/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.a18137036_final;
+
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
